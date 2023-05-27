593f4734485c4a25cdbff590eb8b97b883188cff
stefangohlke <gohlke.stefan@yahoo.de>
2023-05-27 13:51:19 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # express-graphql-cd
 Trying out CD for ExpressJS
+
+## How to run
+To install the necessary dependencies run **npm install**. To run the server execute **npm start**. It will run a Express server using GraphQLServer on localhost:7070/graphql with the user example schema and resolvers from the `@dreamit/graphql-server` project.
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { 
+    GraphQLServer, 
+    JsonLogger 
+} from '@dreamit/graphql-server'
+import { 
+    userSchema, 
+    userSchemaResolvers 
+} from './ExampleSchemas'
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const customGraphQLServer = new GraphQLServer(
+    {
+        schema: userSchema,
+        rootValue: userSchemaResolvers,
+        logger:  new JsonLogger('koaServer', 'user-service'),
+    }
+)
+
+const graphQLServerPort = 7070
+const graphQLServerExpress = express()
+graphQLServerExpress.use(bodyParser.text({type: '*/*'}))
+graphQLServerExpress.all('/graphql', (request, response) => {
+    return customGraphQLServer.handleRequest(request, response)
+})
+graphQLServerExpress.listen({port: graphQLServerPort})
+console.info(`Starting GraphQL server on port ${graphQLServerPort}`)
